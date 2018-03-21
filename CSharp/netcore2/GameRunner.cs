6890f8d7b48c69e7a9f36a843ccfc9d545623e0f
Fabien Vignon <fvignon@linagora.com>
2018-03-21 19:12:28 +0100

    add .net core version

---
+++ CSharp/netcore2/GameRunner.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Trivia
+{
+    public class GameRunner
+    {
+
+        private static bool notAWinner;
+
+        public static void Main(String[] args)
+        {
+            Game aGame = new Game();
+
+            aGame.Add("Chet");
+            aGame.Add("Pat");
+            aGame.Add("Sue");
+
+            Random rand = new Random();
+
+            do
+            {
+
+                aGame.Roll(rand.Next(5) + 1);
+
+                if (rand.Next(9) == 7)
+                {
+                    notAWinner = aGame.WrongAnswer();
+                }
+                else
+                {
+                    notAWinner = aGame.WasCorrectlyAnswered();
+                }
+
+
+
+            } while (notAWinner);
+
+        }
+
+
+    }
+
+}
