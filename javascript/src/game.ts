553477dcbea1082c1ab45a0454799362aaf596e1
Johan Martinsson <j.martinsson@bee-buzziness.com>
2018-05-09 00:04:12 +0200

    fix ts migration

--- javascript/src/game.ts
+++ javascript/src/game.ts
@@ -1,41 +1,60 @@
-var Game = /** @class */ (function () {
-    function Game() {
+class Game {
+
+    rockQuestions: any[];
+    isGettingOutOfPenaltyBox: boolean;
+    currentPlayer: number;
+    sportsQuestions: any[];
+    scienceQuestions: any[];
+    inPenaltyBox: any[];
+    popQuestions: any[];
+    purses: any[];
+    places: any[];
+    players: any[];
+
+    constructor() {
         this.players = new Array();
         this.places = new Array(6);
         this.purses = new Array(6);
         this.inPenaltyBox = new Array(6);
+
         this.popQuestions = new Array();
         this.scienceQuestions = new Array();
         this.sportsQuestions = new Array();
         this.rockQuestions = new Array();
+
         this.currentPlayer = 0;
         this.isGettingOutOfPenaltyBox = false;
-        for (var i = 0; i < 50; i++) {
+
+        for (let i = 0; i < 50; i++) {
             this.popQuestions.push("Pop Question " + i);
             this.scienceQuestions.push("Science Question " + i);
             this.sportsQuestions.push("Sports Question " + i);
             this.rockQuestions.push(this.createRockQuestion(i));
         }
+
     }
-    Game.prototype.add = function (playerName) {
+
+    add(playerName) {
         this.players.push(playerName);
         this.places[this.howManyPlayers() - 1] = 0;
         this.purses[this.howManyPlayers() - 1] = 0;
         this.inPenaltyBox[this.howManyPlayers() - 1] = false;
+
         console.log(playerName + " was added");
         console.log("They are player number " + this.players.length);
+
         return true;
     };
-    ;
-    Game.prototype.howManyPlayers = function () {
+
+    howManyPlayers() {
         return this.players.length;
     };
-    ;
-    Game.prototype.didPlayerWin = function () {
-        return !(this.purses[this.currentPlayer] == 6);
+
+    didPlayerWin() {
+        return !(this.purses[this.currentPlayer] == 6)
     };
-    ;
-    Game.prototype.currentCategory = function () {
+
+    currentCategory() {
         if (this.places[this.currentPlayer] == 0)
             return 'Pop';
         if (this.places[this.currentPlayer] == 4)
@@ -56,16 +75,18 @@ var Game = /** @class */ (function () {
             return 'Sports';
         return 'Rock';
     };
-    ;
-    Game.prototype.createRockQuestion = function (index) {
+
+    createRockQuestion(index) {
         return "Rock Question " + index;
     };
-    ;
-    Game.prototype.isPlayable = function (howManyPlayers) {
+
+
+    isPlayable(howManyPlayers) {
         return howManyPlayers >= 2;
     };
-    ;
-    Game.prototype.askQuestion = function () {
+
+
+    askQuestion() {
         if (this.currentCategory() == 'Pop') {
             console.log(this.popQuestions.shift());
         }
@@ -79,74 +100,84 @@ var Game = /** @class */ (function () {
             console.log(this.rockQuestions.shift());
         }
     };
-    ;
-    Game.prototype.roll = function (roll) {
+
+    roll(roll) {
         console.log(this.players[this.currentPlayer] + " is the current player");
         console.log("They have rolled a " + roll);
+
         if (this.inPenaltyBox[this.currentPlayer]) {
             if (roll % 2 != 0) {
                 this.isGettingOutOfPenaltyBox = true;
+
                 console.log(this.players[this.currentPlayer] + " is getting out of the penalty box");
                 this._movePlayerAndAskQuestion(roll);
-            }
-            else {
+            } else {
                 console.log(this.players[this.currentPlayer] + " is not getting out of the penalty box");
                 this.isGettingOutOfPenaltyBox = false;
             }
-        }
-        else {
+        } else {
+
             this._movePlayerAndAskQuestion(roll);
         }
     };
-    ;
-    Game.prototype._movePlayerAndAskQuestion = function (roll) {
+
+    _movePlayerAndAskQuestion(roll) {
         this.places[this.currentPlayer] = this.places[this.currentPlayer] + roll;
         if (this.places[this.currentPlayer] > 11) {
             this.places[this.currentPlayer] = this.places[this.currentPlayer] - 12;
         }
+
         console.log(this.players[this.currentPlayer] + "'s new location is " + this.places[this.currentPlayer]);
         console.log("The category is " + this.currentCategory());
         this.askQuestion();
-    };
-    Game.prototype._doAnswerCorrectly = function () {
+    }
+
+    _doAnswerCorrectly() {
         console.log('Answer was correct!!!!');
         this.currentPlayer += 1;
         if (this.currentPlayer == this.players.length)
             this.currentPlayer = 0;
+
         this.purses[this.currentPlayer] += 1;
         console.log(this.players[this.currentPlayer] + " now has " +
             this.purses[this.currentPlayer] + " Gold Coins.");
+
         var winner = this.didPlayerWin();
+
         return winner;
-    };
-    Game.prototype.wasCorrectlyAnswered = function () {
+    }
+
+    wasCorrectlyAnswered() {
         if (this.inPenaltyBox[this.currentPlayer]) {
             if (this.isGettingOutOfPenaltyBox) {
                 return this._doAnswerCorrectly();
-            }
-            else {
+            } else {
                 this.currentPlayer += 1;
                 if (this.currentPlayer == this.players.length)
                     this.currentPlayer = 0;
                 return true;
             }
-        }
-        else {
+
+
+        } else {
+
             return this._doAnswerCorrectly();
         }
     };
-    ;
-    Game.prototype.wrongAnswer = function () {
+
+    wrongAnswer() {
         console.log('Question was incorrectly answered');
         console.log(this.players[this.currentPlayer] + " was sent to the penalty box");
         this.inPenaltyBox[this.currentPlayer] = true;
+
         this.currentPlayer += 1;
         if (this.currentPlayer == this.players.length)
             this.currentPlayer = 0;
         return true;
     };
-    ;
-    return Game;
-}());
+
+}
+
 exports = typeof window !== "undefined" && window !== null ? window : global;
-module.exports = Game;
+
+module.exports = Game
